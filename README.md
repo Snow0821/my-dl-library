0aa8f1402bb1c727dd22aa0020f22cb8376ec9ed
unknown <sosaror@gmail.com>
2025-09-15 21:44:46 +0900

    weight update partial computation

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ $L = (y - \hat y)^2$
 # Weight and bias update
 $\hat w = w - \eta \frac{\partial L}{ \partial w}$ \
 $\hat b = b - \eta \frac{\partial L}{\partial b} $
+
+# Weight update
+
+## partial computation
+$\frac{\partial L}{ \partial w} = \frac{\partial (y - \hat y)^2}{\partial w}$ \
+$ = \frac{\partial (y - wx + b)^2}{\partial w}$ \
+$ = 2(y - wx + b)\frac{\partial (y - wx + b)}{\partial w}$ \
+$ = 2x(y - wx + b)$ \
+$ = 2x(y - \hat y)$
+
+## update
+$\hat w = w - 2\eta x (y - \hat y)$
--- module/module.py
+++ module/module.py
@@ -12,3 +12,25 @@ or_dataset = [
     (1, 1, 1),
 ]
 
+def mseloss(y, y_pred):
+    return (y - y_pred) ** 2
+
+class my_and_gate:
+    def __init__(self):
+        self.w1 = 0.5
+        self.w2 = 0.5
+        self.b = 0.5
+        return
+    
+    def forward(self, x1, x2):
+        return self.w1 * x1 + self.w2 * x2 + self.b
+    
+myand = my_and_gate()
+
+loss = 0
+for x1, x2, y in and_dataset:
+    y_pred = myand.forward(x1, x2)
+    loss += mseloss(y, y_pred)
+print(loss / len(and_dataset))
+
+
